d3282657c7702a23a37c739a2ba363cdd5a15b97
Nirojan Selvanathan <niro273@Nirojans-MacBook-Pro.local>
2015-02-01 13:27:52 +0530

    added createAdminFunction for intial load

--- src/com/data/extractor/model/login/LoginRequestProcessor.java
+++ src/com/data/extractor/model/login/LoginRequestProcessor.java
@@ -3,6 +3,7 @@ package com.data.extractor.model.login;
 
 import com.data.extractor.model.beans.authenticate.login.LoginRequest;
 import com.data.extractor.model.beans.authenticate.login.LoginResponse;
+import com.data.extractor.model.data.access.layer.CounterDAO;
 import com.data.extractor.model.data.access.layer.UsersDAO;
 import com.mongodb.MongoClient;
 
@@ -17,7 +18,13 @@ public class LoginRequestProcessor {
         if (usersDAO.isUserExists(loginRequest)){
             loginRequest.setIsAuthenticated(true);
         }else {
-            loginRequest.setErrorCause("Invalid UserName or Password");
+            if(loginRequest.getUserName().equals("admin") && loginRequest.getPass().equals("admin")){
+                createAdminUser(mongoClient,loginRequest);
+                loginRequest.setIsAuthenticated(true);
+            }else {
+                loginRequest.setErrorCause("Invalid UserName or Password");
+            }
+
         }
 
         LoginResponse loginResponse=new LoginResponse();
@@ -27,5 +34,13 @@ public class LoginRequestProcessor {
 
         return loginResponse;
 
+    }
+
+    public void createAdminUser(MongoClient mongoClient,LoginRequest loginRequest){
+        CounterDAO counterDAO = new CounterDAO(mongoClient);
+        UsersDAO usersDAO = new UsersDAO(mongoClient);
+        Integer id = counterDAO.getNextId("userId");
+
+        usersDAO.createUser(id.toString(),loginRequest.getUserName(),loginRequest.getPass(),"admin",loginRequest.getUserName());
     }
 }
