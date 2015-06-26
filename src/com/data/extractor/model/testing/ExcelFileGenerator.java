5a1dd41f85f1ebaf9aaaa5140d81ccbdf602f182
Nirojan Selvanathan <niro273@Nirojans-MacBook-Pro.local>
2015-06-26 08:03:21 +0530

    adding functionality for table data excel generator

--- src/com/data/extractor/model/testing/ExcelFileGenerator.java
+++ src/com/data/extractor/model/testing/ExcelFileGenerator.java
@@ -7,6 +7,9 @@ import com.data.extractor.model.beans.template.info.pattern.PatternDataParser;
 import com.data.extractor.model.beans.template.info.regex.RegexDataElement;
 import com.data.extractor.model.beans.template.info.regex.RegexDataParser;
 import com.data.extractor.model.beans.template.info.regex.RegexPairElement;
+import com.data.extractor.model.beans.template.info.table.Column;
+import com.data.extractor.model.beans.template.info.table.TableDataElement;
+import com.data.extractor.model.beans.template.info.table.TableDataParser;
 import com.data.extractor.model.data.access.layer.ExtractedDataDAO;
 import com.mongodb.MongoClient;
 import org.apache.poi.ss.usermodel.Cell;
@@ -36,6 +39,7 @@ public class ExcelFileGenerator {
 
         sheet = addRecordsForRegex(sheet,extractedDataDAO,nodeId);
         sheet = addRecordsForPattern(sheet,extractedDataDAO,nodeId);
+        sheet = addRecordsForTable(sheet,extractedDataDAO,nodeId);
         String excelPath = writeToExcel(workbook,rootPath,parentId,nodeId);
 
 
@@ -146,6 +150,48 @@ public class ExcelFileGenerator {
 
     }
 
+    public static XSSFSheet addRecordsForTable(XSSFSheet sheet,ExtractedDataDAO extractedDataDAO,String nodeId){
+
+        //List<PatternDataParser> patternDataParsers = extractedDataDAO.getPatternRecord(nodeId);
+        ExtractedData extractedData = testingTable(extractedDataDAO,nodeId);
+
+        List<TableDataElement> tableDataElementList = extractedData.getTableDataElements();
+
+        for (int j=0;j<tableDataElementList.size();j++){
+
+            TableDataElement tableDataElement = tableDataElementList.get(j);
+
+            List<Column> columnList=tableDataElement.getColumns();
+            List<List<String>> rows = tableDataElement.getRows();
+
+
+            // get the column size to iterate
+            Row patternTableHeadRow=sheet.createRow(rowCount++);
+            Row tableColumnHead=sheet.createRow(rowCount++);
+            Cell headTableCell = patternTableHeadRow.createCell(0);
+            headTableCell.setCellValue("Table Data");
+
+            for (int k=0;k<columnList.size();k++){
+                Cell cell =tableColumnHead.createCell(k);
+                cell.setCellValue(columnList.get(k).getMetaName());
+            }
+
+            for (int k=0;k<rows.size();k++){
+
+                Row row=sheet.createRow(rowCount++);
+                Cell cell;
+                List<String> tableRow =rows.get(k);
+                for (int l=0;l<tableRow.size();l++){
+                    cell = row.createCell(l);
+                    cell.setCellValue(tableRow.get(l));
+                }
+            }
+        }
+
+        return sheet;
+
+    }
+
     public static String writeToExcel(XSSFWorkbook workbook ,String rootPath,String parentId,String nodeId){
         String excelPath=null;
         // Make Download Path
@@ -187,6 +233,51 @@ public class ExcelFileGenerator {
         }
         return excelPath;
     }
+
+    public static ExtractedData testingTable(ExtractedDataDAO extractedDataDAO,String nodeId){
+
+
+        List<TableDataParser> tableDataParserList = extractedDataDAO.getTableRecord(nodeId);
+        ExtractedData extractedData=new ExtractedData();
+
+        if (tableDataParserList.size() != 0) {
+            extractedData.setTableDataElements(tableDataParserList.get(0).getTableDataElements());
+            List<TableDataElement> tableDataElements = extractedData.getTableDataElements();
+
+            for(TableDataElement t:tableDataElements){
+                List<Column> columnList = t.getColumns();
+
+
+                List<String> values=new ArrayList<String>();
+                String value=null;
+                Column column;
+
+                int rowSize = columnList.get(0).getCellValues().size();
+                List<List<String>> rowList = new ArrayList<List<String>>();
+                for (int j=0;j<rowSize;j++){
+
+                    for (int k=0;k<columnList.size();k++){
+                        column= columnList.get(k);
+                        value = column.getCellValues().get(j);
+                        values.add(value);
+                    }
+                    rowList.add(values);
+                    values = new ArrayList<String>();
+                }
+                t.setRows(rowList);
+
+                // remove unwanted values
+                for (Column c:columnList){
+                    c.setCellValues(null);
+                }
+            }
+
+        }
+        return extractedData;
+
+    }
+
+
 
     public static ExtractedData testing(ExtractedDataDAO extractedDataDAO,String nodeId){
 
