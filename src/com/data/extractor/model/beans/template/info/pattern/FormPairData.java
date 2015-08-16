03ded31c9ec3be0a806f74d6423eed9276c7def1
nirodev <niro273@Nirojans-MacBook-Pro.local>
2015-08-16 13:41:06 +0530

    adding back end functionality to append patterndata to form

---
+++ src/com/data/extractor/model/beans/template/info/pattern/FormPairData.java
@@ -0,0 +1,26 @@
+package com.data.extractor.model.beans.template.info.pattern;
+
+/**
+ * Created by niro273 on 8/16/15.
+ */
+public class FormPairData {
+
+    private String start;
+    private String end;
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
--- src/com/data/extractor/model/template/markup/MarkUpRequestProcessor.java
+++ src/com/data/extractor/model/template/markup/MarkUpRequestProcessor.java
@@ -2,10 +2,12 @@ package com.data.extractor.model.template.markup;
 
 import com.data.extractor.model.beans.markup.template.MarkUpRequest;
 import com.data.extractor.model.beans.markup.template.MarkUpResponse;
+import com.data.extractor.model.beans.template.info.pattern.FormPairData;
 import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MarkUpRequestProcessor {
     public MarkUpResponse processRequest(String jsonRequest,MongoClient mongoClient) throws IOException {
@@ -38,6 +40,9 @@ public class MarkUpRequestProcessor {
             if(dataType.equals("pattern") ){
                 TextDataExtractor textDataExtractor = new TextDataExtractor();
                 markUpResponse=textDataExtractor.extractText(jsonRequest,mongoClient);
+
+                PatternDataExtractor patternDataExtractor = new PatternDataExtractor();
+                List<FormPairData> pairDatas = patternDataExtractor.processRequest(markUpResponse.getExtractedData());
             }
 
         }
